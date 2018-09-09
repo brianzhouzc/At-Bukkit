b752f0ea60dc7e76a1517a60256f3d1be9e91aab
Bryan_lzh <390807154@qq.com>
2018-09-09 17:32:20 +0800

    修正无法自动补全的问题

--- src/me/Brian/at/Main.java
+++ src/me/Brian/at/Main.java
@@ -19,227 +19,226 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerChatTabCompleteEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	public final Logger logger = Logger.getLogger("Minecraft");
-	public static Main plugin;
-	public final HashMap<Player, Boolean> at = new HashMap<Player, Boolean>();
-	public final List<Player> cooldown = new ArrayList<Player>();
 
-	@Override
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
-		File configFile = new File(getDataFolder(), "config.yml");
-		FileConfiguration config = YamlConfiguration
-				.loadConfiguration(configFile);
-		if (!configFile.exists()) {
-			config.set("sound", 1);
-			config.set("cooldown", 10);
-			try {
-				config.save(configFile);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public final Logger logger = Logger.getLogger("Minecraft");
+    public static Main plugin;
+    public final HashMap<Player, Boolean> at = new HashMap<>();
+    public final List<Player> cooldown = new ArrayList<>();
 
-	@Override
-	public void onDisable() {
-	}
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        File configFile = new File(getDataFolder(), "config.yml");
+        FileConfiguration config = YamlConfiguration
+                .loadConfiguration(configFile);
+        if (!configFile.exists()) {
+            config.set("sound", 1);
+            config.set("cooldown", 10);
+            try {
+                config.save(configFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public boolean onCommand(CommandSender sender, Command cmd, String lable,
-			String[] args) {
-		if (lable.equalsIgnoreCase("at")) {
-			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				if (args.length == 0) {
-					player.sendMessage(ChatColor.AQUA
-							+ "------------------------------");
-					player.sendMessage(ChatColor.AQUA + "/at help : 提示");
-					player.sendMessage(ChatColor.AQUA + "/at disable : 打开免打扰功能");
-					player.sendMessage(ChatColor.AQUA + "/at enable : 关闭免打扰功能");
-					player.sendMessage(ChatColor.AQUA
-							+ "------------------------------");
-					return true;
-				} else if (args.length == 1) {
-					if (args[0].equalsIgnoreCase("help")) {
-						player.sendMessage(ChatColor.AQUA
-								+ "[@] Help:请在输入人名字之后输入空格或者按tab补全");
-						return true;
-					} else if (args[0].equalsIgnoreCase("disable")) {
-						player.sendMessage(ChatColor.AQUA + "[@] 你打开了免打扰功能");
-						at.put(player, false);
-						return true;
-					} else if (args[0].equalsIgnoreCase("enable")) {
-						player.sendMessage(ChatColor.AQUA + "[@] 你关闭了免打扰功能");
-						at.put(player, true);
-						return true;
-					}
-				}
-			} else {
-				sender.sendMessage("[@] 只有玩家才能用@");
-			}
-		}
-		return false;
-	}
+    @Override
+    public void onDisable() {
+    }
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void chat(AsyncPlayerChatEvent event) {
-		String msg = event.getMessage();
-		final Player player = event.getPlayer();
-		int sum = 0;
-		if (msg.contains("@")) {
-			if (!cooldown.contains(player)) {
-				List<Player> ap = new ArrayList<Player>();
-				ap.addAll(Bukkit.getServer().getOnlinePlayers());
-				int sound = getConfig().getInt("sound");
-				int cooldownsec = getConfig().getInt("cooldown");
-				if (player.hasPermission("at.cooldown") || player.isOp()) {
-					cooldownsec = 0;
-				}
-				if (msg.contains("@全体玩家")) {
-					if (player.hasPermission("at.use.all") || player.isOp()) {
-						for (Player target : ap) {
-							if (!(at.containsKey(target) && at.get(target) == false)) {
-								target.sendMessage(ChatColor.AQUA + "[@] "
-										+ player.getDisplayName()
-										+ ChatColor.AQUA + " @了你");
-								switch (sound) {
-								case 1:
-									target.playSound(target.getLocation(),
-											Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
-									break;
-								case 2:
-									target.playSound(target.getLocation(),
-											Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
-									break;
-								case 3:
-									target.playSound(target.getLocation(),
-											Sound.ENTITY_GHAST_SCREAM, 1.0F, 1.0F);
-									break;
-								case 4:
-									target.playSound(target.getLocation(),
-											Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1.0F, 1.0F);
-									break;
-								case 5:
-									target.playSound(target.getLocation(),
-											Sound.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
-									break;
-								default:
-									player.sendMessage(ChatColor.AQUA
-											+ "[@] 请联系op,@插件配置有误");
-									break;
-								}
-							}
-						}
+    public boolean onCommand(CommandSender sender, Command cmd, String lable,
+            String[] args) {
+        if (lable.equalsIgnoreCase("at")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length == 0) {
+                    player.sendMessage(ChatColor.AQUA
+                            + "------------------------------");
+                    player.sendMessage(ChatColor.AQUA + "/at help : 提示");
+                    player.sendMessage(ChatColor.AQUA + "/at disable : 打开免打扰功能");
+                    player.sendMessage(ChatColor.AQUA + "/at enable : 关闭免打扰功能");
+                    player.sendMessage(ChatColor.AQUA
+                            + "------------------------------");
+                    return true;
+                } else if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("help")) {
+                        player.sendMessage(ChatColor.AQUA
+                                + "[@] Help:请在输入人名字之后输入空格或者按tab补全");
+                        return true;
+                    } else if (args[0].equalsIgnoreCase("disable")) {
+                        player.sendMessage(ChatColor.AQUA + "[@] 你打开了免打扰功能");
+                        at.put(player, false);
+                        return true;
+                    } else if (args[0].equalsIgnoreCase("enable")) {
+                        player.sendMessage(ChatColor.AQUA + "[@] 你关闭了免打扰功能");
+                        at.put(player, true);
+                        return true;
+                    }
+                }
+            } else {
+                sender.sendMessage("[@] 只有玩家才能用@");
+            }
+        }
+        return false;
+    }
 
-						msg = msg.replaceAll("@全体玩家", ChatColor.AQUA + "@全体玩家"
-								+ ChatColor.RESET);
-						sum++;
-					} else {
-						player.sendMessage(ChatColor.AQUA + "[@] 你没有权限@全体玩家！");
-					}
-				} else {
-					if (player.hasPermission("at.use") || player.isOp()) {
-						for (Player target : ap) {
-							if (msg.endsWith("@" + target.getName())
-									|| msg.contains("@" + target.getName()
-											+ " ")) {
-								msg = msg.replaceAll("@" + target.getName(),
-										ChatColor.AQUA + "@" + target.getName()
-												+ ChatColor.RESET);
-								if (!(at.containsKey(target) && at.get(target) == false)) {
-									target.sendMessage(ChatColor.AQUA + "[@] "
-											+ player.getDisplayName()
-											+ ChatColor.AQUA + "@了你");
-									switch (sound) {
-									case 1:
-										target.playSound(target.getLocation(),
-												Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
-										break;
-									case 2:
-										target.playSound(target.getLocation(),
-												Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
-										break;
-									case 3:
-										target.playSound(target.getLocation(),
-												Sound.ENTITY_GHAST_SCREAM, 1.0F, 1.0F);
-										break;
-									case 4:
-										target.playSound(target.getLocation(),
-												Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1.0F, 1.0F);
-										break;
-									case 5:
-										target.playSound(target.getLocation(),
-												Sound.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
-										break;
-									default:
-										player.sendMessage(ChatColor.AQUA
-												+ "[@] 请联系op,@插件配置有误");
-										break;
-									}
-								}
-							}
-							sum++;
-						}
-						if (sum == 0) {
-							player.sendMessage(ChatColor.AQUA + "[@] "
-									+ "玩家不在线或不存在");
-						}
-					} else {
-						player.sendMessage(ChatColor.AQUA + "[@] 你没有权限使用@");
-					}
-				}
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void chat(AsyncPlayerChatEvent event) {
+        String msg = event.getMessage();
+        final Player player = event.getPlayer();
+        int sum = 0;
+        if (msg.contains("@")) {
+            if (!cooldown.contains(player)) {
+                List<Player> ap = new ArrayList<Player>();
+                ap.addAll(Bukkit.getServer().getOnlinePlayers());
+                int sound = getConfig().getInt("sound");
+                int cooldownsec = getConfig().getInt("cooldown");
+                if (player.hasPermission("at.cooldown") || player.isOp()) {
+                    cooldownsec = 0;
+                }
+                if (msg.contains("@全体玩家")) {
+                    if (player.hasPermission("at.use.all") || player.isOp()) {
+                        for (Player target : ap) {
+                            if (!(at.containsKey(target) && at.get(target) == false)) {
+                                target.sendMessage(ChatColor.AQUA + "[@] "
+                                        + player.getDisplayName()
+                                        + ChatColor.AQUA + " @了你");
+                                switch (sound) {
+                                    case 1:
+                                        target.playSound(target.getLocation(),
+                                                Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
+                                        break;
+                                    case 2:
+                                        target.playSound(target.getLocation(),
+                                                Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
+                                        break;
+                                    case 3:
+                                        target.playSound(target.getLocation(),
+                                                Sound.ENTITY_GHAST_SCREAM, 1.0F, 1.0F);
+                                        break;
+                                    case 4:
+                                        target.playSound(target.getLocation(),
+                                                Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1.0F, 1.0F);
+                                        break;
+                                    case 5:
+                                        target.playSound(target.getLocation(),
+                                                Sound.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
+                                        break;
+                                    default:
+                                        player.sendMessage(ChatColor.AQUA
+                                                + "[@] 请联系op,@插件配置有误");
+                                        break;
+                                }
+                            }
+                        }
 
-				if (sum != 0) {
-					event.setMessage(msg);
-					cooldown.add(player);
-					Bukkit.getServer().getScheduler()
-							.scheduleSyncDelayedTask(this, new Runnable() {
-								public void run() {
-									cooldown.remove(player);
-								}
-							}, cooldownsec * 20);
+                        msg = msg.replaceAll("@全体玩家", ChatColor.AQUA + "@全体玩家"
+                                + ChatColor.RESET);
+                        sum++;
+                    } else {
+                        player.sendMessage(ChatColor.AQUA + "[@] 你没有权限@全体玩家！");
+                    }
+                } else {
+                    if (player.hasPermission("at.use") || player.isOp()) {
+                        for (Player target : ap) {
+                            if (msg.endsWith("@" + target.getName())
+                                    || msg.contains("@" + target.getName()
+                                            + " ")) {
+                                msg = msg.replaceAll("@" + target.getName(),
+                                        ChatColor.AQUA + "@" + target.getName()
+                                        + ChatColor.RESET);
+                                if (!(at.containsKey(target) && at.get(target) == false)) {
+                                    target.sendMessage(ChatColor.AQUA + "[@] "
+                                            + player.getDisplayName()
+                                            + ChatColor.AQUA + "@了你");
+                                    switch (sound) {
+                                        case 1:
+                                            target.playSound(target.getLocation(),
+                                                    Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
+                                            break;
+                                        case 2:
+                                            target.playSound(target.getLocation(),
+                                                    Sound.ENTITY_GENERIC_EXPLODE, 1.0F, 1.0F);
+                                            break;
+                                        case 3:
+                                            target.playSound(target.getLocation(),
+                                                    Sound.ENTITY_GHAST_SCREAM, 1.0F, 1.0F);
+                                            break;
+                                        case 4:
+                                            target.playSound(target.getLocation(),
+                                                    Sound.ENTITY_FIREWORK_ROCKET_TWINKLE, 1.0F, 1.0F);
+                                            break;
+                                        case 5:
+                                            target.playSound(target.getLocation(),
+                                                    Sound.ENTITY_WITHER_DEATH, 1.0F, 1.0F);
+                                            break;
+                                        default:
+                                            player.sendMessage(ChatColor.AQUA
+                                                    + "[@] 请联系op,@插件配置有误");
+                                            break;
+                                    }
+                                }
+                            }
+                            sum++;
+                        }
+                        if (sum == 0) {
+                            player.sendMessage(ChatColor.AQUA + "[@] "
+                                    + "玩家不在线或不存在");
+                        }
+                    } else {
+                        player.sendMessage(ChatColor.AQUA + "[@] 你没有权限使用@");
+                    }
+                }
 
-				}
-			} else {
-				int cooldownsec = getConfig().getInt("cooldown");
-				player.sendMessage(ChatColor.AQUA + "[@] 冷却中，冷却时间为"
-						+ cooldownsec + "秒");
-			}
-		}
-	}
+                if (sum != 0) {
+                    event.setMessage(msg);
+                    cooldown.add(player);
+                    Bukkit.getServer().getScheduler()
+                            .scheduleSyncDelayedTask(this, new Runnable() {
+                                public void run() {
+                                    cooldown.remove(player);
+                                }
+                            }, cooldownsec * 20);
 
-	@EventHandler
-	public void onTabComplete(PlayerChatTabCompleteEvent event) {
-		String uncomplete = event.getChatMessage();
-		if (uncomplete.contains("@") && !uncomplete.endsWith(" ")) {
-			List<Player> allplayers = new ArrayList<Player>();
-			allplayers.addAll(Bukkit.getServer().getOnlinePlayers());
-			String at = uncomplete.substring(uncomplete.lastIndexOf("@") + 1);
-			List<String> fit = new ArrayList<String>();
-			for (Player p : allplayers) {
-				if (p.getName().toLowerCase().startsWith(at.toLowerCase())) {
-					if (uncomplete.contains(" ")) {
-						fit.add(uncomplete.substring(
-								uncomplete.lastIndexOf(" ") + 1,
-								uncomplete.lastIndexOf("@"))
-								+ "@" + p.getName());
-						// event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
-					} else {
-						fit.add(uncomplete.substring(0,
-								uncomplete.lastIndexOf("@"))
-								+ "@" + p.getName());
-						// event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
-					}
-				}
-			}
-			event.getTabCompletions().clear();
-			event.getTabCompletions().addAll(fit);
-			// event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
-		}
-	}
+                }
+            } else {
+                int cooldownsec = getConfig().getInt("cooldown");
+                player.sendMessage(ChatColor.AQUA + "[@] 冷却中，冷却时间为"
+                        + cooldownsec + "秒");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onTabComplete(TabCompleteEvent event) {
+        String uncomplete = event.getBuffer();
+        if (uncomplete.contains("@") && !uncomplete.endsWith(" ")) {
+            String at = uncomplete.substring(uncomplete.lastIndexOf("@") + 1);
+            List<String> fit = new ArrayList<>();
+            for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+                if (p.getName().toLowerCase().startsWith(at.toLowerCase())) {
+                    if (uncomplete.contains(" ")) {
+                        fit.add(uncomplete.substring(
+                                uncomplete.lastIndexOf(" ") + 1,
+                                uncomplete.lastIndexOf("@"))
+                                + "@" + p.getName());
+                        // event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
+                    } else {
+                        fit.add(uncomplete.substring(0,
+                                uncomplete.lastIndexOf("@"))
+                                + "@" + p.getName());
+                        // event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
+                    }
+                }
+            }
+            event.getCompletions().clear();
+            event.getCompletions().addAll(fit);
+            // event.getPlayer().sendMessage(ChatColor.RED+event.getTabCompletions().toString()+ChatColor.GREEN+event.getLastToken());
+        }
+    }
 
 }
